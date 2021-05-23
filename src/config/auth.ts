1c41c3feb0bf9f6acf2cb21f05e4828df9347a1d
Silvio Ronaldo <silvio.ronaldo77@gmail.com>
2021-05-23 00:42:22 -0300

    Definindo tipo da Secret

--- src/config/auth.ts
+++ src/config/auth.ts
@@ -1,6 +1,13 @@
+import { Secret } from 'jsonwebtoken';
+
+type JWTProps = {
+    secret: Secret;
+    expiresIn: string;
+};
+
 export default {
     jwt: {
         secret: process.env.SECRET,
         expiresIn: process.env.EXPIRES,
-    },
+    } as JWTProps,
 };
