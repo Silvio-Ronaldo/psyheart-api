de40e5a8a6c36f83f5fc23b64466f6a05d3ab387
Silvio Ronaldo <silvio.ronaldo77@gmail.com>
2021-05-29 01:05:37 -0300

    Utilizando ts-node-dev no deploy

--- ormconfig.js
+++ ormconfig.js
@@ -8,10 +8,10 @@ module.exports = {
         },
     },
     entities: [
-        "./dist/modules/**/infra/typeorm/entities/*.ts"
+        "./src/modules/**/infra/typeorm/entities/*.ts"
     ],
     migrations: [
-        "./dist/shared/infra/typeorm/migrations/*.ts"
+        "./src/shared/infra/typeorm/migrations/*.ts"
     ],
     cli: {
         "migrationsDir": "./src/shared/infra/typeorm/migrations"
