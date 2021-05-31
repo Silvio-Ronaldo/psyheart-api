40eb162ee84c9d2248abd677335201b4de6e9cf9
Silvio Ronaldo <silvio.ronaldo77@gmail.com>
2021-05-30 22:13:29 -0300

    Adicionando controllers

--- ormconfig.js
+++ ormconfig.js
@@ -1,6 +1,6 @@
 module.exports = {
     type: "postgres",
-    url: process.env.DATABASE_URL,
+    url: process.env.DATABASE_LOCAL,
     ssl: true,
     extra: {
         ssl: {
--- src/modules/users/infra/http/routes/users.routes.ts
+++ src/modules/users/infra/http/routes/users.routes.ts
@@ -1,52 +1,24 @@
 import { Router } from 'express';
 import multer from 'multer';
-import { container } from 'tsyringe';
 
-import CreateUserService from '../../../services/CreateUserService';
-import UpdateUserAvatarService from '../../../services/UpdateUserAvatarService';
+import UsersController from '../controllers/UsersController';
+import UserAvatarController from '../controllers/UserAvatarController';
 
 import ensureAuthenticated from '../middlewares/ensureAuthenticated';
 import uploadConfig from '../../../../../config/upload';
 
 const usersRouter = Router();
 const upload = multer(uploadConfig);
+const usersController = new UsersController();
+const userAvatarController = new UserAvatarController();
 
-usersRouter.post('/', async (request, response) => {
-    try {
-        const { name, email, password } = request.body;
-
-        const createUser = container.resolve(CreateUserService);
-
-        const user = await createUser.execute({
-            name,
-            email,
-            password,
-        });
-
-        delete user.password;
-
-        return response.json(user);
-    } catch (err) {
-        return response.status(400).json({ error: err.message });
-    }
-});
+usersRouter.post('/', usersController.create);
 
 usersRouter.patch(
     '/avatar',
     ensureAuthenticated,
     upload.single('avatar'),
-    async (request, response) => {
-        const updateUserAvatar = container.resolve(UpdateUserAvatarService);
-
-        const user = await updateUserAvatar.execute({
-            user_id: request.user.id,
-            avatarFilename: request.file.filename,
-        });
-
-        delete user.password;
-
-        return response.json(user);
-    },
+    userAvatarController.update,
 );
 
 export default usersRouter;
