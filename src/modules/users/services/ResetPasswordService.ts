505620027ccad90b97c4b7cfa2c6f0914f302455
Silvio Ronaldo <silvio.ronaldo77@gmail.com>
2021-06-03 04:49:42 -0300

    Criando recuperacao e reset de senha com tdd

---
+++ src/modules/users/services/ResetPasswordService.ts
@@ -0,0 +1,42 @@
+import { injectable, inject } from 'tsyringe';
+
+import IUsersRepository from '../repositories/IUsersRepository';
+import IUserTokensRepository from '../repositories/IUserTokensRepository';
+
+import AppError from '../../../shared/errors/AppError';
+
+interface IRequest {
+    token: string;
+    newPassword: string;
+}
+
+@injectable()
+class ResetPasswordService {
+    constructor(
+        @inject('UsersRepository')
+        private usersRepository: IUsersRepository,
+
+        @inject('UserTokensRepository')
+        private userTokensRepository: IUserTokensRepository,
+    ) {}
+
+    public async execute({ token, newPassword }: IRequest): Promise<void> {
+        const userToken = await this.userTokensRepository.findByToken(token);
+
+        if (!userToken) {
+            throw new AppError('User token does not exists');
+        }
+
+        const user = await this.usersRepository.findById(userToken.user_id);
+
+        if (!user) {
+            throw new AppError('User does not exists');
+        }
+
+        user.password = newPassword;
+
+        await this.usersRepository.save(user);
+    }
+}
+
+export default ResetPasswordService;
