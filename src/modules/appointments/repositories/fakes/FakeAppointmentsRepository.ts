e9d8e2fbb2c7dd046f39e0dbf952fbd0c20336cb
Silvio Ronaldo <silvio.ronaldo77@gmail.com>
2021-06-11 03:35:27 -0300

    Testando a disponibilidade por dia do provider

--- src/modules/appointments/repositories/fakes/FakeAppointmentsRepository.ts
+++ src/modules/appointments/repositories/fakes/FakeAppointmentsRepository.ts
@@ -1,9 +1,10 @@
 import { v4 as uuid } from 'uuid';
-import { isEqual, getMonth, getYear } from 'date-fns';
+import { isEqual, getMonth, getYear, getDate } from 'date-fns';
 
 import IAppointmentsRepository from '../IAppointmentsRepository';
 import ICreateAppointmentDTO from '../../dtos/ICreateAppointmentDTO';
 import IFindAllInMonthFromProviderDTO from '../../dtos/IFindAllInMonthFromProviderDTO';
+import IFindAllInDayFromProviderDTO from '../../dtos/IFindAllInDayFromProviderDTO';
 
 import Appointment from '../../infra/typeorm/entities/Appointment';
 
@@ -39,6 +40,24 @@ class FakeAppointmentsRepository implements IAppointmentsRepository {
         const appointments = this.appointments.filter(appointment => {
             return (
                 appointment.provider_id === provider_id &&
+                getMonth(appointment.date) + 1 === month &&
+                getYear(appointment.date) === year
+            );
+        });
+
+        return appointments;
+    }
+
+    public async findAllInDayFromProvider({
+        provider_id,
+        day,
+        month,
+        year,
+    }: IFindAllInDayFromProviderDTO): Promise<Appointment[]> {
+        const appointments = this.appointments.filter(appointment => {
+            return (
+                appointment.provider_id === provider_id &&
+                getDate(appointment.date) === day &&
                 getMonth(appointment.date) + 1 === month &&
                 getYear(appointment.date) === year
             );
