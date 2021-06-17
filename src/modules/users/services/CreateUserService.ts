f71154b4660a3c28884747aa947c01932ac6c475
Silvio Ronaldo <silvio.ronaldo77@gmail.com>
2021-06-17 03:24:04 -0300

    Invalidando todos os caches com o mesmo prefixo

--- src/modules/users/services/CreateUserService.ts
+++ src/modules/users/services/CreateUserService.ts
@@ -3,6 +3,7 @@ import { injectable, inject } from 'tsyringe';
 import User from '../infra/typeorm/entities/User';
 import IUsersRepository from '../repositories/IUsersRepository';
 import IHashProvider from '../providers/HashProvider/models/IHashProvider';
+import ICacheProvider from '../../../shared/container/providers/CacheProvider/models/ICacheProvider';
 
 import AppError from '../../../shared/errors/AppError';
 
@@ -20,6 +21,9 @@ class CreateUserService {
 
         @inject('HashProvider')
         private hashProvider: IHashProvider,
+
+        @inject('CacheProvider')
+        private cacheProvider: ICacheProvider,
     ) {}
 
     public async execute({
@@ -40,6 +44,8 @@ class CreateUserService {
             email,
             password: hashedPassword,
         });
+
+        await this.cacheProvider.invalidatePrefix('providers-list');
 
         return user;
     }
