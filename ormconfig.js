d693968e4a11b8c1e7fc46c1dd6a8251b19da308
Silvio Ronaldo <silvio.ronaldo77@gmail.com>
2021-06-30 01:53:52 -0300

    Retornando o ormconfig

---
+++ ormconfig.js
@@ -0,0 +1,30 @@
+module.exports = [
+    {
+        type: "postgres",
+        url: process.env.DATABASE_URL,
+        ssl: true,
+        extra: {
+            ssl: {
+                rejectUnauthorized: false
+            },
+        },
+        entities: [
+            "./dist/modules/**/infra/typeorm/entities/*.js"
+        ],
+        migrations: [
+            "./dist/shared/infra/typeorm/migrations/*.js"
+        ],
+        cli: {
+            "migrationsDir": "./src/shared/infra/typeorm/migrations"
+        }
+    },
+    {
+        name: "mongo",
+        type: "mongodb",
+        url: process.env.DATABASE_DEV_MONGO,
+        useUnifiedTopology: true,
+        entities: [
+            "./src/modules/**/infra/typeorm/schemas/*.ts"
+        ]
+    }
+]
